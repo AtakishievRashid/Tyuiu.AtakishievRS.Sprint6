2700a1cc81e1c3234375e5767b81ff0261a50d1f
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-12-13 01:50:03 +0500

    Task6

--- Tyuiu.AtakishievRS.Sprint6.Task3.V1/FormMain.Designer.cs
+++ Tyuiu.AtakishievRS.Sprint6.Task3.V1/FormMain.Designer.cs
@@ -28,12 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            groupBoxCon = new GroupBox();
+            dataGridViewMTRX_ARS = new DataGridView();
+            textBox1 = new TextBox();
+            buttonHelp_ARS = new Button();
+            buttonDone_ARS = new Button();
+            groupBox1 = new GroupBox();
+            dataGridViewResult = new DataGridView();
+            groupBoxCon.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMTRX_ARS).BeginInit();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResult).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBoxCon
+            // 
+            groupBoxCon.Controls.Add(dataGridViewMTRX_ARS);
+            groupBoxCon.Controls.Add(textBox1);
+            groupBoxCon.Font = new Font("Segoe UI", 12F);
+            groupBoxCon.Location = new Point(12, 12);
+            groupBoxCon.Name = "groupBoxCon";
+            groupBoxCon.Size = new Size(537, 361);
+            groupBoxCon.TabIndex = 0;
+            groupBoxCon.TabStop = false;
+            groupBoxCon.Text = "Условие";
+            // 
+            // dataGridViewMTRX_ARS
+            // 
+            dataGridViewMTRX_ARS.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewMTRX_ARS.Location = new Point(261, 28);
+            dataGridViewMTRX_ARS.Name = "dataGridViewMTRX_ARS";
+            dataGridViewMTRX_ARS.Size = new Size(256, 230);
+            dataGridViewMTRX_ARS.TabIndex = 1;
+            dataGridViewMTRX_ARS.CellContentClick += dataGridViewMTRX_ARS_CellContentClick;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.Menu;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 10F);
+            textBox1.Location = new Point(10, 33);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(222, 257);
+            textBox1.TabIndex = 0;
+            textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // buttonHelp_ARS
+            // 
+            buttonHelp_ARS.Location = new Point(6, 307);
+            buttonHelp_ARS.Name = "buttonHelp_ARS";
+            buttonHelp_ARS.Size = new Size(37, 37);
+            buttonHelp_ARS.TabIndex = 1;
+            buttonHelp_ARS.Text = "?";
+            buttonHelp_ARS.UseVisualStyleBackColor = true;
+            buttonHelp_ARS.Click += buttonHelp_ARS_Click;
+            // 
+            // buttonDone_ARS
+            // 
+            buttonDone_ARS.Location = new Point(61, 307);
+            buttonDone_ARS.Name = "buttonDone_ARS";
+            buttonDone_ARS.Size = new Size(137, 37);
+            buttonDone_ARS.TabIndex = 2;
+            buttonDone_ARS.Text = "Выполнить";
+            buttonDone_ARS.UseVisualStyleBackColor = true;
+            buttonDone_ARS.Click += buttonDone_ARS_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dataGridViewResult);
+            groupBox1.Controls.Add(buttonDone_ARS);
+            groupBox1.Controls.Add(buttonHelp_ARS);
+            groupBox1.Font = new Font("Segoe UI", 10F);
+            groupBox1.Location = new Point(555, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(301, 361);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Вывод данных";
+            // 
+            // dataGridViewResult
+            // 
+            dataGridViewResult.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewResult.Location = new Point(33, 28);
+            dataGridViewResult.Name = "dataGridViewResult";
+            dataGridViewResult.Size = new Size(248, 230);
+            dataGridViewResult.TabIndex = 3;
+            dataGridViewResult.CellContentClick += dataGridViewResult_CellContentClick;
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(868, 385);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBoxCon);
+            Name = "FormMain";
+            Text = "Спринт 6 | Таск 3 | Вариант 1 | Атакишиев Р.С.";
+            Load += FormMain_Load;
+            groupBoxCon.ResumeLayout(false);
+            groupBoxCon.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMTRX_ARS).EndInit();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResult).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBoxCon;
+        private TextBox textBox1;
+        private Button buttonHelp_ARS;
+        private Button buttonDone_ARS;
+        private GroupBox groupBox1;
+        private DataGridView dataGridViewMTRX_ARS;
+        private DataGridView dataGridViewResult;
     }
 }
--- Tyuiu.AtakishievRS.Sprint6.Task3.V1/FormMain.cs
+++ Tyuiu.AtakishievRS.Sprint6.Task3.V1/FormMain.cs
@@ -1,3 +1,6 @@
+using System.Data.Common;
+using System.Windows.Forms;
+using Tyuiu.AtakishievRS.Sprint6.Task3.V1.Lib;
 namespace Tyuiu.AtakishievRS.Sprint6.Task3.V1
 {
     public partial class FormMain : Form
@@ -5,6 +8,74 @@ namespace Tyuiu.AtakishievRS.Sprint6.Task3.V1
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        DataService ds = new DataService();
+        int[,] mtrx = new int[5, 5] { { 25, 20, 28, 2, 6},
+                                      { 19, 9, 18, -7, 24},
+                                      { 21, 16, 5, -7, -8},
+                                      { 30, -13, 1, -15, 2 },
+                                      { 31, 9, -3, -9, -18 } };
+        private void buttonHelp_ARS_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 3 выполнил студент группы РППб-25-1 Атакишиев Рашид Саркар оглы", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonDone_ARS_Click(object sender, EventArgs e)
+        {
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.GetUpperBound(1) + 1;
+
+            dataGridViewResult.ColumnCount = columns;
+            dataGridViewResult.RowCount = rows;
+
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewResult.Columns[i].Width = 40;
+            }
+            int[,] res = ds.Calculate(mtrx);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewResult.Rows[i].Cells[j].Value = Convert.ToString(res[i, j]);
+                }
+            }
+        }   
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.GetUpperBound(1) + 1;
+
+            dataGridViewMTRX_ARS.ColumnCount = columns;
+            dataGridViewMTRX_ARS.RowCount = rows;
+
+            for (int i = 0; i < columns; i++)
+            {
+                dataGridViewMTRX_ARS.Columns[i].Width = 40;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    dataGridViewMTRX_ARS.Rows[i].Cells[j].Value = Convert.ToString(mtrx[i, j]);
+                }
+            }
+        }
+
+        private void dataGridViewMTRX_ARS_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridViewResult_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
