9ab23a79a1cfaa04ae5444b8f82ee4c3cb28c0bf
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-12-12 19:43:04 +0500

    Task4

---
+++ Tyuiu.AtakishievRS.Sprint6.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.AtakishievRS.Sprint6.Task4.V3.Lib
+{
+    public class DataService : ISprint6Task4V3
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int count = stopValue - startValue + 1;
+            double[] results = new double[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                double x = startValue + i;
+                double den = Math.Sin(x) - 3 + x;
+
+                if (Math.Abs(den) < 0.000001)
+                {
+                    results[i] = 0;
+                }
+                else
+                {
+                    double num = 3 * x - 1.5;
+                    double value = num / den + 2;
+                    results[i] = Math.Round(value, 2);
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint6.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.AtakishievRS.Sprint6.Task4.V3.Lib;
+namespace Tyuiu.AtakishievRS.Sprint6.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var ds = new DataService();
+            var result = ds.GetMassFunction(-5, 5);
+
+            double[] expected = {
+        -0.18, -0.22, -0.28, -0.37, -0.54,
+        1.50, 0.28, 0.15, 0.09, 0.06, 0.04
+    };
+
+            Assert.AreEqual(expected.Length, result.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], result[i], 0.001);
+            }
+        }
+    }
+}
